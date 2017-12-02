167b5b9ff136b2b5ed872ba7f4819b8d37ef1cbf
marco-oliveira <marco-oliveira@live.com>
2017-12-02 12:34:10 -0200

    Commit do projeto inteiro...

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,78 @@
+class NegociacaoController{
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(),
+                                         new NegociacoesView($('#negociacoesView')),
+                                            'adiciona', 'esvazia');
+        this._mensagem = new Bind(new Mensagem(),
+                                new MensagemView($('#mesagemView')),
+                                    'texto');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociacao adicionada com sucesso';
+        this._limpaFormulario();
+
+    }
+
+    importaNegociacoes(){
+
+        let service = new NegociacaoService();
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações Importadas com sucesso!';
+        });
+
+        service.obterNegociacoesDaSemanaAnterior((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações Importadas com sucesso!';
+        });
+
+        service.obterNegociacoesDaSemanaRetrasada((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações Importadas com sucesso!';
+        });
+    }
+
+    apaga(){
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = "Negociações removidas com sucesso";
+    }
+
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputValor.value = 0,0;
+        this._inputQuantidade.value = 1;
+        this._inputData.focus();
+    }
+}
---
+++ client/js/app/services/NegociacaoService.js
@@ -0,0 +1,62 @@
+class NegociacaoService{
+
+    obterNegociacoesDaSemana(cb){
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semana');
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                   cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Erro ao Importar Negociações', null);
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    obterNegociacoesDaSemanaAnterior(cb){
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/anterior');
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Erro ao Importar Negociações', null);
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    obterNegociacoesDaSemanaRetrasada(cb){
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/retrasada');
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Erro ao Importar Negociações', null);
+                }
+            }
+        };
+        xhr.send();
+    }
+}
