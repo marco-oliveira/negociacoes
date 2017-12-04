9f58cf7dac56cec21ad472bab9666dbe1044b576
marco-oliveira <marco-oliveira@live.com>
2017-12-04 21:41:30 -0200

    Criando Fábrica de conexão.

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,47 @@
+
+var connection = null;
+var dbName = 'aluraframe';
+var version = 4;
+var stores = ['negociacoes'];
+
+class ConnectionFactory{
+
+    constructor(){
+
+        throw new Error('A classe ConnectionFactory não pode ser instanciada!');
+    }
+
+    static getConnection(){
+
+        return new Promise((resolve, reject) => {
+
+            let openRequest = window.indexedDB.open(dbName, version);
+
+            openRequest.onupgradeneeded = e => {
+                ConnectionFactory._createStores(e.target.result);
+            };
+
+            openRequest.onsuccess = e => {
+
+                if (!connection) connection = e.target.result;
+                resolve(connection);
+            };
+
+            openRequest.onerror = e => {
+
+                console.log(e.target.error);
+                reject(e.target.error.name);
+            };
+
+        });
+    }
+
+    static _createStores(connection){
+        stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store);
+            connection.createObjectStore(store, { autoIncrement : true });
+        })
+    }
+
+}
